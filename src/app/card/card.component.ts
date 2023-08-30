eb0ec42b437b544be4e33fbed41df9e9603dfa4f
hamadaelbassel <hamadaelbassel@gmail.com>
2023-08-30 20:43:59 +0300

    refactoring card component

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Country } from '../modals/country.modal';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent {
+  @Input('country') country!: Country;
+  @Output('click') click = new EventEmitter();
 
+  navigateToDetials(country: Country) {
+    this.click.emit(country);
+  }
 }
